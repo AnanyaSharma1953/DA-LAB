0aae21fcd1e6e2c95d5a70e14443825ab9784ba2
AnanyaSharma1953 <156696581+AnanyaSharma1953@users.noreply.github.com>
2024-10-01 09:19:06 +0530

    Create JOSEPHUS(LINKEDLIST).c

---
+++ JOSEPHUS(LINKEDLIST).c
@@ -0,0 +1,72 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct list
+{
+	int d;
+	struct list * next;
+}s;
+void inser(s**,s**);
+void kill(s*,int);
+//void disp(s*);
+int main()
+{
+	int ch;
+	s *f=NULL,*r=NULL;
+	do
+	{
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				inser(&f,&r);
+				break;
+			/*case 2:
+				disp(f);
+				break;
+				*/
+			case 3:
+				kill(f,2);
+		}
+	}while(ch<=3);	
+}
+void kill(s* f, int k) {
+    if (f->next == f) {
+        printf("The survivor is person number %d\n", f->d);
+        free(f); 
+        return;
+    } else {
+        s* t = f;
+        int y = 1;
+        while (y < k-1) {
+            y++;
+            t = t->next;
+        }
+        s* to = t->next;
+        t->next = to->next;
+        printf("Person number %d is eliminated.\n", to->d);
+        free(to);
+        kill(t->next, k);
+    }
+}
+
+void inser(s **f,s **r)
+{
+	s *p=NULL;
+	p=(s*)malloc(sizeof(s));
+	if(p!=NULL)
+	{
+		int x;
+		scanf("%d",&x);
+		p->d=x;
+		if(*f==NULL&&*r==NULL)
+			*f=*r=p;
+		else
+		{
+			(*r)->next=p;
+			*r=p;
+		}
+		(*r)->next=(*f);
+	}
+	
+}
